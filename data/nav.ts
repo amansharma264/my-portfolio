0eeebd38e738ef8facbd0392a3256a8f2805cbcf
amansharma264 <amankumar170789@gmail.com>
2026-07-14 11:26:56 +0530

    Remove navigation and social links data files

--- data/nav.ts
+++
@@ -1,9 +0,0 @@
-import { Navlink } from "@/types";
-
-export const navLinks: NavLink[] = [
-  { label: "Home", href: "/" },
-  { label: "About", href: "/about" },
-  { label: "Projects", href: "/projects" },
-  { label: "Blog", href: "/blog" },
-  { label: "Guestbook", href: "/guestbook" },
-];
--- data/socials.ts
+++
@@ -1,8 +0,0 @@
-import { SocialLink } from "@/types";
-
-export const socialLinks: SocialLink[] = [
-  { platform: "github", url: "https://github.com/amansharma264", icon: "github" },
-  { platform: "linkedin", url: "https://linkedin.com/in/aman-kumar-239920273/", icon: "linkedin" },
-];
-
-export const githubUsername = "amansharma264";
